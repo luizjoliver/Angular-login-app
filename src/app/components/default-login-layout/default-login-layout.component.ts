07c49100f3f25f6ba08b3ee885e71a886c9382d6
luizjoliver <luizfellipejoliver14@gmail.com>
2025-04-26 15:38:53 -0300

    Finalização default login layout com estilização

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
   imports: [],
   templateUrl: './default-login-layout.component.html',
-  styleUrl: './default-login-layout.component.scss'
+  styleUrl: './default-login-layout.component.scss',
 })
 export class DefaultLoginLayoutComponent {
-
+  @Input({ required: true }) title: string = '';
+  @Input({ required: true }) primaryBtnText: string = '';
+  @Input({ required: true }) secondaryBtnText: string = '';
 }
