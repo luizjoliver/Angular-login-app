408f4f2600f4d24c635f2eb34226630b5df3a187
luizjoliver <luizfellipejoliver14@gmail.com>
2025-04-26 17:17:10 -0300

    Adding ngx toast to add toast to our app and his dependencies

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
@@ -10,4 +10,13 @@ export class DefaultLoginLayoutComponent {
   @Input({ required: true }) title: string = '';
   @Input({ required: true }) primaryBtnText: string = '';
   @Input({ required: true }) secondaryBtnText: string = '';
+  @Output('submit') onSubmit = new EventEmitter()
+  @Output('navigate') onNavigate = new EventEmitter()
+
+  submit(){
+    this.onSubmit.emit()
+  }
+  navigate(){
+    this.onNavigate.emit()
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from "../../components/primary-input/primary-input.component";
-
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   imports: [DefaultLoginLayoutComponent,
@@ -13,10 +14,28 @@ import { PrimaryInputComponent } from "../../components/primary-input/primary-in
 export class LoginComponent {
   loginForm!:FormGroup
 
-  constructor(){
+  constructor(private router:Router, private toastr: ToastrService,){
     this.loginForm = new FormGroup({
       email:new FormControl('',[Validators.required,Validators.email]),
       password:new FormControl('',[Validators.required,Validators.minLength(6)])
     })
+  }
+
+
+
+  submit(){
+    this.toastr.success(
+      `Email: ${this.loginForm.value.email}<br>
+       Senha: ${this.loginForm.value.password}<br>
+       Pronto para enviar ao backend!`,
+      'Dados do Login',
+      {
+        enableHtml: true,
+        timeOut: 5000
+      }
+    )
+  }
+  navigate(){
+    this.router.navigate(['/signup'])
   }
 }
